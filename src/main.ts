8a84cd11e76915f488de00ef629561c9dc821bbc
Estela Pinto Requejo <erequejo@companywatch.net>
2025-03-19 14:49:44 +0100

    Clean

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
-import { RankingComponent } from './app/ranking/ranking.component';
 import { DataService } from './app/data.service';
 
 bootstrapApplication(AppComponent, {
